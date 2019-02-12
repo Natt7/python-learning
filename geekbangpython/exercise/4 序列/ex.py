9c7d860863f240b339aaf115757b901bc2952fd0
Natt7 <natt.ni@outlook.com>
2019-02-12 21:35:13 +0800

    Update ex.py

--- geekbangpython/exercise/4 序列/ex.py
+++ geekbangpython/exercise/4 序列/ex.py
@@ -1,11 +1,28 @@
-a = 'Hello python'
-print(a)
-b = "Let's go"
-print(b)
-c = '"The Zen of Python" -- by Tim Peters '
-print(c)
+# a = 'Hello python'
+# print(a)
+# b = "Let's go"
+# print(b)
+# c = '"The Zen of Python" -- by Tim Peters '
+# print(c)
+#
+# c = 'xyz'
+# d = 'abc'
+# print(c + d)
+# print(c[1:8])
+# print(d * 10)
+#
+# print('a' not in c + d)
+#
+# list5 = [1,3,4,5,6]
+# list5.append(100)
+# print(list5)
+# list5.remove(3)
+# print(list5)
+# print(list5[0:3])
+# print(list5[-1])
 
-c = 'xyz'
-d = 'abc'
-print(c + d)
-print(c[1:3])
+tuple1 = (1, 3, 4, 5, 6)
+# tuple1.append()
+print(tuple1[-2])
+tuple2 = (3, 1, 6, 0, 11)
+print(len(tuple1 + tuple2))
